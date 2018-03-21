66f3e3b22024b833d0f97f8f0735f02c6b7d32f9
Michelle Rodrigues <mica.msr@gmail.com>
2018-03-20 22:39:58 -0300

    ajuste na inicialização do banco

--- crud/Data/DBInicializar.cs
+++ crud/Data/DBInicializar.cs
@@ -12,16 +12,19 @@ namespace Crud.Data
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
                 var context = serviceScope.ServiceProvider.GetService<AppDataContext>();
+                if(context.Database.EnsureCreated())
+                {
+                    var produto1 = new Produto { Nome="Mini Garrafa Térmica Gourmet", Valor=835}; 
+                    var produto2 = new Produto { Nome="Cabideiro de Aço Inoxidável", Valor=635}; 
+                    var produto3 = new Produto { Nome="Porta Copos Gourmet", Valor=395};
+
+                    context.Produtos.Add(produto1);
+                    context.Produtos.Add(produto2);
+                    context.Produtos.Add(produto3);
+
+                    context.SaveChanges();
+                }
                 
-                var produto1 = new Produto { Nome="Mini Garrafa Térmica Gourmet", Valor=835}; 
-                var produto2 = new Produto { Nome="Cabideiro de Aço Inoxidável", Valor=635}; 
-                var produto3 = new Produto { Nome="Porta Copos Gourmet", Valor=395};
-
-                context.Produtos.Add(produto1);
-                context.Produtos.Add(produto2);
-                context.Produtos.Add(produto3);
-
-                context.SaveChanges();
             }
         }
     }
